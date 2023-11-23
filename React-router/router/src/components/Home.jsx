72774314189bed182be2f7190a886d80ed20e891
FlavioAraujo23 <flaviolopes1027@gmail.com>
2023-11-23 20:33:36 -0300

    add lesson head

--- React-router/router/src/components/Home.jsx
+++ React-router/router/src/components/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Head from './Head'
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
   return (
-    <div>Essa é a Home</div>
+//     Helmet
+// Uma extensão famosa é o react-helmet. Ela retonar com componente em que você pode definir tags do Head dentro do mesmo.
+    <div>
+       <Helmet>
+        <title>Página Home</title>
+        <meta name="description" content="Conteúdo da descrição" />
+      </Helmet>
+      <Head title="Página Home" description="Descrição da home" />
+      <p>Essa é a Home</p>
+    </div>
   )
 }
 
